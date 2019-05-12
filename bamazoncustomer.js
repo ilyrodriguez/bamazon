45c9668278e9d6ccd2fc03c00f7b8f1b191a68f1
unknown <ilyrodriguez@gmail.com>
2019-05-12 00:01:49 -0400

    Table works

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,5 +1,10 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var colors = require('colors');
+var Table = require('cli-table');
+           
+var headlineDivider = "\n" + "~~~~~~~~~~~~~~~~~~~~~~~".bgCyan.black  + "\n";
+var divider = "\n" + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n";
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -13,40 +18,51 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Welcome")
-  displayTable();
+  if (err) throw err;  
+//   console.log("Connected as id:", connection.threadId);
+ 
+  console.log(headlineDivider + "||".bgCyan.black + "      Welcome      ".green.bold + "||".bgCyan.black + "\n" + 
+  "||".bgCyan.black +"        to         ".green.bold + "||".bgCyan.black + "\n" + 
+  "||".bgCyan.black + "      BAMAZON      ".green.bold + "||".bgCyan.black + headlineDivider);
 });
 
+// var productArr = [];
+
 function displayTable() {
-    connection.query("SELECT * FROM products", function(error, results) {
+    var table = new Table({
+        // head: ['DEALS OF THE MONTH'.bold],
+        head: ['ID'.bold.cyan, 'Product Name'.bold.green, 'Department'.bold.green, 'Price'.bold.green, 'Stock Quantity'.bold.green],
+    });
+    connection.query("SELECT * FROM products", function(error, res) {
       if (error) throw error;
-      for (i = 0; i < results.length; i++) {
-          console.log("id number: " + results[i].id +"\n"+"product name: "+ results[i].product_name + "\n"+"price: " + results[i].price + line);
-  
-      }
-  
-    //   purchase();
-      connection.end();
+      for (i = 0; i < res.length; i++) {
+        table.push([res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+        //   console.log("id number: " + res[i].id +"\n"+"product name: "+ res[i].product_name + "\n" + "price: $" + res[i].price + divider),
+        //   productArr.push("id number: " + res[i].id +"\n"+"product name: "+ res[i].product_name + "\n"+ "price: $" + res[i].price + divider)
+       
+    }
+    console.log(table.toString());
+    console.log(divider);
+
+    PickProduct()
     });
   };
-// function start() {
-//     inquirer
-//       .prompt({
-//         name: "Welcome",
-//         type: "list",
-//         message: "Welcome to BAMAZON!" + "\n" + "Please pick a product to purchase: ",
-//         choices: ["POST", "BID", "EXIT"]
-//       })
-//       .then(function(answer) {
-//         // based on their answer, either call the bid or the post functions
-//         if (answer.postOrBid === "POST") {
-//           postAuction();
-//         }
-//         else if(answer.postOrBid === "BID") {
-//           bidAuction();
-//         } else{
-//           connection.end();
-//         }
-//       });
-  }
+  displayTable();
+
+    
+function PickProduct() {
+    inquirer
+      .prompt({
+        name: "PickID",
+        type: "number",
+        message: "\n" + "Please type ID number to add your product in the cart: " + "\n",
+        // choices: productArr
+      },{
+        name: "quantity",
+        type: "number",
+        message: "\n" + "How many would you like to purchase?" + "\n",
+        // choices: productArr
+    })
+      .then(function(answer) {
+       
+      connection.end();})}
